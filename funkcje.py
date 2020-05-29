b7486f979f6d0a519b0668eb6d6b70719306af69
PGszafir <pgszafir@gmail.com>
2020-05-29 16:52:15 +0300

    kompresja i korekcja kodu

--- funkcje.py
+++ funkcje.py
@@ -1,6 +1,13 @@
 def fun1():
   print('spis funkcji')
 
+def iter3(tab,znak,pole,nr):
+    a=0
+    for j in range(3):
+        if(tab[pole+j*nr]==znak):
+            a+=1
+    return(a,pole,znak)
+
 
 class Plansza:
     def __init__(self):
@@ -10,59 +17,39 @@ class Plansza:
         for i in range(0, 9):
             if (i % 3 == 0):
                 print('\n')
-            print(tab[i], '|', end='')
+            print(self.tab[i], '|', end='')
 
+    def sprawdz(self):
 
-
-
-
-
-
-
-
-
-
-
-
-plwzorc = []
-for i in range(0, 9):
-    plwzorc.append(i + 1)
-
+        # LINIA +1
+        # PION +3
+        # SKOS LEWO +2
+        # SKOS PRAWO +4
+        zn = ['O', 'X']
+        for z in zn:
+            for i in range(0, 7, 3):  # +1
+                a = iter3(self.tab, z, i, 1)
+                print(a)
+                if a[0] == 3:
+                    return (a)
+            for i in range(3):
+                b = iter3(self.tab, z, i, 2)
+                print(b)
+                if b[0] == 3:
+                    return b
+            for i in range(0, 3, 2):
+                c = iter3(self.tab, z, i, 4 - i)
+                print(c)
+                if c[0] == 3:
+                    return c
+        return max(a, b, c)
 
 
 # def pytaj(i):
 # i=int(input('gracz',i,'proszę podać nr pola'))
 
-def iter3(tab,znak,pole,nr):
-    a=0
-    for j in range(3):
-        if(tab[pole+j*nr]==znak):
-            a+=1
-    return(a,pole,znak)
 
-def sprawdz(tablica):
-    zn=['O','X']
-    for z in zn:
-        for i in range(0,7,3):#+1
-            a=iter3(tablica,z,i,1)
-            print(a)
-            if a[0]==3:
-                return(a)
-        for i in range(3):
-            b=iter3(tablica, z, i, 2)
-            print(b)
-            if b[0]==3:
-                return b
-        for i in range(0,3,2):
-            c=iter3(tablica, z, i,4-i)
-            print(c)
-            if c[0]==3:
-                return c
-    return max(a,b,c)
-# LINIA +1
-# PION +3
-# SKOS LEWO +2
-# SKOS PRAWO +4
+
 def wczytajPole(plansza,znak):
     text=['\ngracz ', znak, 'proszę podać nr pola']
     gracz1 = int(input(text))
@@ -71,8 +58,10 @@ def wczytajPole(plansza,znak):
     else:
         print('error')
         wczytajPole(plansza,znak)
+
+
 def ruchKomp(plansza,znak):
-    a=sprawdz(plansza)
+    a=plansza.sprawdz()
     if(a[2]==znak):
         print('gracz',znak,'blisko wygranej')
     else:
@@ -80,8 +69,9 @@ def ruchKomp(plansza,znak):
 
 
 def pierwszy_etap(plansza):
+    plansza=Plansza()
     print("to pierwszy estp gry w którym kazdy z graczy może napirzemian dołozyć 3 pionki")
-    rys_plansza(plwzorc)
+    plansza.rys_plansza()
     znak=input('wybierz znak jakim chcesz grać O lób X')
     if znak=='X':
         wczytajPole(plansza,'X')
