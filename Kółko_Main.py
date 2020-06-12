3aa7919ffa4583465db9009b3cd59fdab9892afa
PGszafir <pgszafir@gmail.com>
2020-06-12 18:08:52 +0300

    zmiany

--- Kółko_Main.py
+++ Kółko_Main.py
@@ -1,21 +1,16 @@
 import funkcje
-#import pygame
+import pygame
 import sys
 import random
 from pygame.locals import *
-print('kolko-krzyzyk')
+#'''
+print('kolko-krzyzyk \nPoczątek Gry')
 
 pl0 = [' ' for i in range(9)]
 pl1 = [' ' for i in range(9)]
 
-print('wersja z komp')
-
-funkcje.pierwszy_etap(pl1)
-
-
-
-
-'''
+funkcje.pierwszy_etap(pl1)#'''
+#'''
 pygame.init()
 OKNOGRY = pygame.display.set_mode((500, 500), 0, 32)
 pygame.display.set_caption('Kółko i krzyżyk')
@@ -24,5 +19,4 @@ pygame.display.set_caption('Kółko i krzyżyk')
 RUCH = 1
 WYGRANY = 0  # wynik gry: 0 - nikt, 1 - gracz, 2 - komputer, 3 - remis
 WYGRANA = False
-'''
-
+#'''
