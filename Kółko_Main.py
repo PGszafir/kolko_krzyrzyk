cc28a239ee0c0c3113e37f6afeee2dd096a1e1f2
PGszafir <pgszafir@gmail.com>
2020-06-18 15:28:25 +0300

    git

--- Kółko_Main.py
+++ Kółko_Main.py
@@ -9,8 +9,17 @@ print('kolko-krzyzyk \nPoczątek Gry')
 pl0 = [' ' for i in range(9)]
 pl1 = [' ' for i in range(9)]
 
-funkcje.pierwszy_etap(pl1)#'''
+a=funkcje.pierwszy_etap(pl1)
+#print(a[0].rys_plansza())
+if a==0:
+    print("KONIEC")
+else:
+    funkcje.drugi_etap(*a)
 #'''
+
+
+
+'''
 pygame.init()
 OKNOGRY = pygame.display.set_mode((500, 500), 0, 32)
 pygame.display.set_caption('Kółko i krzyżyk')
@@ -19,4 +28,4 @@ pygame.display.set_caption('Kółko i krzyżyk')
 RUCH = 1
 WYGRANY = 0  # wynik gry: 0 - nikt, 1 - gracz, 2 - komputer, 3 - remis
 WYGRANA = False
-#'''
+'''
